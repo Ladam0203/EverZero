28b668a2601d81ca5616355aa4663c3ddc51c477
Ádám Lőrincz <adam7605@easv365.dk>
2025-02-08 11:22:46 +0100

    .

--- deployable/InvoiceService/Services/Interfaces/IInvoiceService.cs
+++ deployable/InvoiceService/Services/Interfaces/IInvoiceService.cs
@@ -1,10 +1,11 @@
 using InvoiceService.Core;
+using InvoiceService.Core.DTOs;
 
 namespace InvoiceService.Services;
 
 public interface IInvoiceService
 {
-    Task<IEnumerable<Invoice>> GetInvoicesByUserId(Guid userId);
+    Task<IEnumerable<GetInvoiceDTO>> GetInvoicesByUserId(Guid userId);
     //Task<Invoice> GetInvoice(Guid id);
     //Task<Invoice> CreateInvoice(Invoice invoice);
     //Task<Invoice> UpdateInvoice(Invoice invoice);
--- deployable/InvoiceService/Services/InvoiceService.cs
+++ deployable/InvoiceService/Services/InvoiceService.cs
@@ -1,4 +1,5 @@
 using InvoiceService.Core;
+using InvoiceService.Core.DTOs;
 using InvoiceService.Repository;
 
 namespace InvoiceService.Services;
@@ -12,8 +13,27 @@ public class InvoiceService : IInvoiceService
         _invoiceRepository = invoiceRepository;
     }
 
-    public async Task<IEnumerable<Invoice>> GetInvoicesByUserId(Guid userId)
+    public async Task<IEnumerable<GetInvoiceDTO>> GetInvoicesByUserId(Guid userId)
     {
-        return await _invoiceRepository.GetInvoicesByUserId(userId);
+        var invoices = await _invoiceRepository.GetInvoicesByUserId(userId);
+        
+        // TODO: Add AutoMapper
+        return invoices.Select(i => new GetInvoiceDTO
+        {
+            Id = i.Id,
+            Subject = i.Subject,
+            SupplierName = i.SupplierName,
+            BuyerName = i.BuyerName,
+            Date = i.Date,
+            UserId = i.UserId,
+            Lines = i.Lines.Select(l => new GetInvoiceLineDTO
+            {
+                Id = l.Id,
+                InvoiceId = l.InvoiceId,
+                Description = l.Description,
+                Quantity = l.Quantity,
+                Unit = l.Unit
+            }).ToList()
+        });
     }
 }
